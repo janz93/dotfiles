ef873fe2b7465f435716deea29971a05aa133cac
Jan Zaydowicz <jan.zaydowicz@googlemail.com>
2023-02-08 11:22:05 +0100

    feat: configure dock applications
    with the help of dockutil we can automatically set the wanted
    application to the mac dock panel

---
+++ macos/dock.sh
@@ -0,0 +1,14 @@
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/System/Applications/Launchpad.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/KeeWeb.app"
+dockutil --no-restart --add "/Users/janzaydowicz/Downloads/"
+
+killall Dock
