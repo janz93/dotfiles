0902d2c4abed6664f073e1d22e254ea9e6ee6c27
Jan Zaydowicz <jan.zaydowicz@googlemail.com>
2023-02-08 11:27:49 +0100

    chore: add missing setup and sort alphabetically

--- config/vscode/extentions.sh
+++ config/vscode/extentions.sh
@@ -1,2 +1,3 @@
-code --install-extension wmaurer.change-case
+code --install-extension eamodio.gitlens
 code --install-extension ms-vscode-remote.remote-containers
+code --install-extension wmaurer.change-case
